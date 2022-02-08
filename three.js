31acf6d0bce378dc433b88ef569794c92b74706f
siam456 <hasibul15-11038@diu.edu.bd>
2022-02-08 19:26:46 +0600

    dynamic scalling

--- three.js
+++ three.js
@@ -1,6 +1,6 @@
 //importing necessary things
 import * as THREE from "three";
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
 
 import { GLTFLoader } from "./files/Loader.js";
 import { FBXLoader } from "./files/Loader.js";
@@ -11,6 +11,7 @@ let camera,
     scene,
     renderer,
     obj = "",
+    mixer,
     controls;
 let add = 10,
     maxAngleForOrbit = 3.4,
@@ -18,12 +19,12 @@ let add = 10,
     zoomMax = 70,
     zoomMin = 10;
 
+const clock = new THREE.Clock();
+
 init();
 render();
 
-function resizeCanvasToDisplaySize() {
-    
-  }
+function resizeCanvasToDisplaySize() {}
 
 function init() {
     //getting canvas
@@ -37,63 +38,57 @@ function init() {
     renderer = new THREE.WebGLRenderer({
         antialias: true,
         canvas: canvReference,
-        preserveDrawingBuffer: true 
+        preserveDrawingBuffer: true,
     });
     renderer.setPixelRatio(window.devicePixelRatio);
     const canvas = renderer.domElement;
     // look up the size the canvas is being displayed
     const width = canvas.clientWidth;
     const height = canvas.clientHeight;
-    
-      //setup camera
+
+    //setup camera
     camera = new THREE.PerspectiveCamera(
         45,
         window.innerWidth / window.innerHeight,
         1,
         1000
-
     );
 
-    camera.lookAt( scene.position );
+    camera.lookAt(scene.position);
     camera.position.set(0, 0, 25);
 
-   
-  
     // adjust displayBuffer size to match
     if (canvas.width !== width || canvas.height !== height) {
-      // you must pass false here or three.js sadly fights the browser
-      renderer.setSize(width, height, false);
-      camera.aspect = width / height;
-      camera.updateProjectionMatrix();
-  
-      // update any render target sizes here
+        // you must pass false here or three.js sadly fights the browser
+        renderer.setSize(width, height, false);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+
+        // update any render target sizes here
     }
 
     //setup light
-    let light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 0.8 );
-    
+    let light = new THREE.HemisphereLight(0xffffbb, 0x080820, 0.8);
+
     scene.add(light);
 
     let DirectionalLightbt = new THREE.DirectionalLight(0xffffff, 0.7);
-    DirectionalLightbt.position.set( 3, -8, 1.5);
+    DirectionalLightbt.position.set(3, -8, 1.5);
 
     scene.add(DirectionalLightbt);
 
     let DirectionalLightside = new THREE.DirectionalLight(0xffffff, 0.5);
-    DirectionalLightside.position.set( 7, 8, 0);
+    DirectionalLightside.position.set(7, 8, 0);
 
     scene.add(DirectionalLightside);
 
     let DirectionalLightside2 = new THREE.DirectionalLight(0xffffff, 0.5);
-    DirectionalLightside2.position.set( -7, 8, 0);
+    DirectionalLightside2.position.set(-7, 8, 0);
 
     scene.add(DirectionalLightside2);
 
-
     // const helper = new THREE.DirectionalLightHelper( DirectionalLightbt, 5 );
     // scene.add( helper );
-
-
 
     // file handle start
     let file = null,
@@ -105,15 +100,21 @@ function init() {
 
     FBXloader.load("asset/fire.fbx", function (fbx) {
         obj = fbx;
-        obj.scale.set(4, 4, 4);
+        var bbox = new THREE.Box3().setFromObject(obj);
+        var size = bbox.getSize(new THREE.Vector3());
+
+        var maxAxis = Math.max(size.x, size.y, size.z);
+        // console.log(maxAxis);
+        // obj.position.set(0, -5, 0);
+        obj.scale.multiplyScalar(6 / maxAxis);
+
         scene.add(obj);
     });
-
 
     //events for file uploader start
     const inputElement = document.getElementById("fileInput");
     inputElement.addEventListener("change", handleFiles, false);
-    
+
     function handleFiles() {
         const fileList = this.files;
         file = fileList[0];
@@ -128,31 +129,80 @@ function init() {
 
         if (fileExt === "fbx") {
             scene.remove(obj);
-            // console.log(x);
             FBXloader.load(x, function (fbx) {
                 obj = fbx;
-                // obj.position.set(0, -5, 0);
-                obj.scale.set(0.01, 0.01, 0.01);
+                var bbox = new THREE.Box3().setFromObject(obj);
+                var size = bbox.getSize(new THREE.Vector3());
+
+                var maxAxis = Math.max(size.x, size.y, size.z);
+
+                if (!isNaN(maxAxis)) {
+                    obj.scale.multiplyScalar(10 / maxAxis);
+                } else {
+                    obj.scale.set(0.02, 0.02, 0.02);
+                }
+
+                if(obj.animations.length){
+                    mixer = new THREE.AnimationMixer(obj);
+                    const action = mixer.clipAction(obj.animations[0]);
+                    action.play();
+                    obj.castShadow = true;
+                }
+                
                 scene.add(obj);
             });
         } else if (fileExt === "glb" || fileExt === "gltf") {
-            // console.log(obj);
+            console.log(obj);
             scene.remove(obj);
             GLTFloader.load(x, function (gltf) {
                 obj = gltf.scene;
-                obj.scale.set(1, 1, 1);
+                var bbox = new THREE.Box3().setFromObject(obj);
+                var cent = bbox.getCenter(new THREE.Vector3());
+                var size = bbox.getSize(new THREE.Vector3());
+
+                var maxAxis = Math.max(size.x, size.y, size.z);
+                if (!isNaN(maxAxis)) {
+                    obj.scale.multiplyScalar(10 / maxAxis);
+                } else {
+                    obj.scale.set(0.02, 0.02, 0.02);
+                }
+                bbox.getCenter(cent);
+
+                if(obj.animations.length){
+                    mixer = new THREE.AnimationMixer(obj);
+                    const action = mixer.clipAction(obj.animations[0]);
+                    action.play();
+                    obj.castShadow = true;
+                }
+
                 scene.add(obj);
             });
         } else if (fileExt === "obj") {
             scene.remove(obj);
             OBJloader.load(x, function (objNew) {
                 obj = objNew;
-                obj.scale.set( 4, 4, 4 );
+                var bbox = new THREE.Box3().setFromObject(obj);
+                var size = bbox.getSize(new THREE.Vector3());
+
+                var maxAxis = Math.max(size.x, size.y, size.z);
+
+                if (!isNaN(maxAxis)) {
+                    obj.scale.multiplyScalar(10 / maxAxis);
+                } else {
+                    obj.scale.set(0.02, 0.02, 0.02);
+                }
+
+                if(obj.animations.length){
+                    mixer = new THREE.AnimationMixer(obj);
+                    const action = mixer.clipAction(obj.animations[0]);
+                    action.play();
+                    obj.castShadow = true;
+                }
+
                 scene.add(obj);
             });
         }
     });
-    
 
     //events for file uploader end
 
@@ -171,19 +221,49 @@ function init() {
 
         let fileName = link.split(".");
         let fileExt = fileName[fileName.length - 1];
-        
+
         if (fileExt === "fbx") {
             scene.remove(obj);
             FBXloader.load(x, function (fbx) {
                 obj = fbx;
-                obj.scale.set(4,4,4);
+                var bbox = new THREE.Box3().setFromObject(obj);
+                var size = bbox.getSize(new THREE.Vector3());
+
+                var maxAxis = Math.max(size.x, size.y, size.z);
+
+                if (!isNaN(maxAxis)) {
+                    obj.scale.multiplyScalar(10 / maxAxis);
+                } else {
+                    obj.scale.set(0.02, 0.02, 0.02);
+                }
+                if(obj.animations.length){
+                    mixer = new THREE.AnimationMixer(obj);
+                    const action = mixer.clipAction(obj.animations[0]);
+                    action.play();
+                    obj.castShadow = true;
+                }
                 scene.add(obj);
             });
         } else if (fileExt === "glb" || fileExt === "gltf") {
             scene.remove(obj);
             GLTFloader.load(x, function (gltf) {
                 obj = gltf.scene;
-                obj.scale.set(1, 1, 1);
+                var bbox = new THREE.Box3().setFromObject(obj);
+                var size = bbox.getSize(new THREE.Vector3());
+
+                var maxAxis = Math.max(size.x, size.y, size.z);
+
+                if (!isNaN(maxAxis)) {
+                    obj.scale.multiplyScalar(10 / maxAxis);
+                } else {
+                    obj.scale.set(0.02, 0.02, 0.02);
+                }
+                if(obj.animations.length){
+                    mixer = new THREE.AnimationMixer(obj);
+                    const action = mixer.clipAction(obj.animations[0]);
+                    action.play();
+                    obj.castShadow = true;
+                }
                 scene.add(obj);
             });
         } else if (fileExt === "obj") {
@@ -191,15 +271,28 @@ function init() {
             OBJloader.load(x, function (objNew) {
                 obj = objNew;
                 // console.log(obj);
-                obj.scale.set(1, 1, 1);
+                var bbox = new THREE.Box3().setFromObject(obj);
+                var size = bbox.getSize(new THREE.Vector3());
+
+                var maxAxis = Math.max(size.x, size.y, size.z);
+
+                if (!isNaN(maxAxis)) {
+                    obj.scale.multiplyScalar(10 / maxAxis);
+                } else {
+                    obj.scale.set(0.02, 0.02, 0.02);
+                }
+                if(obj.animations.length){
+                    mixer = new THREE.AnimationMixer(obj);
+                    const action = mixer.clipAction(obj.animations[0]);
+                    action.play();
+                    obj.castShadow = true;
+                }
                 scene.add(obj);
             });
         }
     });
-    
 
     //events for url uploader end
-
 
     // file handle end
 
@@ -219,16 +312,15 @@ function init() {
 }
 
 function onWindowResize() {
-    
     const canvas = renderer.domElement;
-    
+
     // look up the size the canvas is being displayed
     const width = canvas.clientWidth;
     const height = canvas.clientHeight;
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
 
-   renderer.setSize(width, height, false);
+    renderer.setSize(width, height, false);
 
     // renderer.setSize(window.innerWidth, window.innerHeight, false);
     render();
@@ -246,6 +338,9 @@ function render() {
 
     if (controls.maxPolarAngle !== maxAngleForOrbit)
         controls.maxPolarAngle = maxAngleForOrbit;
+
+    const delta = clock.getDelta();
+    if (mixer) mixer.update(delta);
 
     controls.update();
     renderer.render(scene, camera);
@@ -277,5 +372,4 @@ export {
     renderer,
     camera,
     setAdd,
-   
 };
